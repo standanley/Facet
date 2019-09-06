93598df1628857975655a8fc8a29740ad60603d0
Daniel Stanley <dstanley@Stanford.edu>
2019-09-06 15:20:06 -0700

    Installing fault from zip archive

---
+++ install_fault.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://github.com/leonardt/fault/archive/2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
+unzip 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
+cd fault-2e80fa9eb445d144e906d0c8071d17128aa4518f
+pip install -e .
+cd ..
+rm 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
+
