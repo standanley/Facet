f6caa9e23a6cea160783e2cc4579458c2f3bd45c
Daniel Stanley <dstanley@Stanford.edu>
2019-09-06 15:42:27 -0700

    Install fault a directory above

--- install_fault.sh
+++ install_fault.sh
@@ -3,9 +3,10 @@
 #wget https://github.com/leonardt/fault/archive/2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
 #unzip 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
 #cd fault-2e80fa9eb445d144e906d0c8071d17128aa4518f
+pushd ..
 git clone https://github.com/leonardt/fault.git
 cd fault
 pip install -e .
-cd ..
+popd
 #rm 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
 
