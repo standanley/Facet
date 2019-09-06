b4b426ba75e0083cc2dcc16246081bf1c1e87c7f
Daniel Stanley <dstanley@Stanford.edu>
2019-09-06 15:37:41 -0700

    Switched to public github url for cloning fault

--- install_fault.sh
+++ install_fault.sh
@@ -3,7 +3,7 @@
 #wget https://github.com/leonardt/fault/archive/2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
 #unzip 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
 #cd fault-2e80fa9eb445d144e906d0c8071d17128aa4518f
-git clone git@github.com:leonardt/fault.git
+git clone https://github.com/leonardt/fault.git
 cd fault
 pip install -e .
 cd ..
