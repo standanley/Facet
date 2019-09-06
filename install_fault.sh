b244e0e631d3025a223a45a4bd48d78e0cbb645d
Daniel Stanley <dstanley@Stanford.edu>
2019-09-06 15:24:56 -0700

    Install fault from git source

--- install_fault.sh
+++ install_fault.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-wget https://github.com/leonardt/fault/archive/2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
-unzip 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
-cd fault-2e80fa9eb445d144e906d0c8071d17128aa4518f
+#wget https://github.com/leonardt/fault/archive/2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
+#unzip 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
+#cd fault-2e80fa9eb445d144e906d0c8071d17128aa4518f
+git clone git@github.com:leonardt/fault.git
+cd fault
 pip install -e .
 cd ..
-rm 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
+#rm 2e80fa9eb445d144e906d0c8071d17128aa4518f.zip
 
